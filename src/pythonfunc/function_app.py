3856e52bca5e190e76d99fef81f5c2b1a013d32e
Janne Mattila <janne.mattila@hotmail.fi>
2025-09-18 18:37:29 +0300

    Update demo

--- src/pythonfunc/function_app.py
+++ src/pythonfunc/function_app.py
@@ -1,11 +1,6 @@
-import io
 import azure.functions as func
-import datetime
-import json
-import logging
-import tempfile
-import os
 from markitdown import MarkItDown
+import io
 
 app = func.FunctionApp()
 
@@ -14,27 +9,3 @@ def Converter(req: func.HttpRequest) -> func.HttpResponse:
     md = MarkItDown()
     result = md.convert_stream(io.BytesIO(req.get_body()))
     return func.HttpResponse(result.text_content, mimetype="text/markdown")
-
-    # # read raw uploaded bytes from the request
-    # uploaded_bytes = req.get_body()
-    # if not uploaded_bytes:
-    #     return func.HttpResponse("No file uploaded", status_code=400)
-
-    # # create a temporary file (Windows-safe) and write the upload to disk
-    # suffix = ".xlsx"  # adjust suffix if needed or infer from request headers
-    # tmp_path = None
-    # try:
-    #     with tempfile.NamedTemporaryFile(delete=False, suffix=suffix) as tmp:
-    #         tmp.write(uploaded_bytes)
-    #         tmp_path = tmp.name
-
-    #     logging.info("Converting temporary file: %s", tmp_path)
-    #     result = md.convert(tmp_path)
-    # finally:
-    #     if tmp_path and os.path.exists(tmp_path):
-    #         try:
-    #             os.remove(tmp_path)
-    #         except Exception:
-    #             logging.exception("Failed to remove temp file %s", tmp_path)
-
-    # return func.HttpResponse(result.text_content, mimetype="text/markdown")
