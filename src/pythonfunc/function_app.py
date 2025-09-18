3ee70704d4019510e7ee7b1911d319701e8f3ad7
Janne Mattila <janne.mattila@hotmail.fi>
2025-09-18 21:51:12 +0300

    Update demo

--- src/pythonfunc/function_app.py
+++ src/pythonfunc/function_app.py
@@ -4,7 +4,8 @@ import io
 
 app = func.FunctionApp()
 
-@app.route(route="Converter", auth_level=func.AuthLevel.ANONYMOUS)
+@app.function_name(name="Converter")
+@app.route(route="", auth_level=func.AuthLevel.ANONYMOUS)
 def Converter(req: func.HttpRequest) -> func.HttpResponse:
     md = MarkItDown()
     result = md.convert_stream(io.BytesIO(req.get_body()))
